2a60a938aaa10ff7e865a4072a277202dfdcb14b
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2023-07-03 11:51:30 -0500

    Feat: I Am Poor completed

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Leidy Luna on 3/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
